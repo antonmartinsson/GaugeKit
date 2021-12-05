3e20c11effa653dff63a12b838084172fc1c4c8c
Anton Martinsson <aejmartinsson@gmail.com>
2021-12-05 11:58:28 +0100

    Updates tests and README.

--- Tests/GaugeKitTests/GaugeKitTests.swift
+++ Tests/GaugeKitTests/GaugeKitTests.swift
@@ -9,21 +9,38 @@ final class GaugeKitTests: XCTestCase {
 		XCTAssertNotNil(gauge.value)
 		XCTAssertEqual(gauge.title, "A title")
 		XCTAssertEqual(gauge.value, 100)
+		XCTAssertEqual(gauge.maxValue, 100)
 		XCTAssertEqual(gauge.colors, [.red, .green])
+		XCTAssertNil(gauge.additionalInfo)
+	}
+	
+	func testMaxValueInit() {
+		let gauge = GaugeView(title: "A title", value: 100, maxValue: 200, colors: [.red, .green])
+		XCTAssertNotNil(gauge.title)
+		XCTAssertNotNil(gauge.value)
+		XCTAssertEqual(gauge.title, "A title")
+		XCTAssertEqual(gauge.value, 100)
+		XCTAssertEqual(gauge.maxValue, 200)
+		XCTAssertEqual(gauge.colors, [.red, .green])
+		XCTAssertNil(gauge.additionalInfo)
 	}
 	
 	func testNoTitleOrValue() {
 		let gauge = GaugeView(colors: [.red, .green])
 		XCTAssertNil(gauge.title)
 		XCTAssertNil(gauge.value)
+		XCTAssertEqual(gauge.maxValue, 0)
 		XCTAssertNotNil(gauge.colors)
+		XCTAssertNil(gauge.additionalInfo)
 	}
 	
 	func testNoColors() {
 		let gauge = GaugeView(colors: [])
 		XCTAssertNil(gauge.title)
 		XCTAssertNil(gauge.value)
+		XCTAssertEqual(gauge.maxValue, 0)
 		XCTAssertTrue(gauge.colors.isEmpty)
+		XCTAssertNil(gauge.additionalInfo)
 	}
 	
 	func testAdditionalInfo() {
@@ -34,6 +51,7 @@ final class GaugeKitTests: XCTestCase {
 		
 		XCTAssertEqual(gauge.title, "A title")
 		XCTAssertEqual(gauge.value, 100)
+		XCTAssertEqual(gauge.maxValue, 100)
 		XCTAssertEqual(gauge.colors, [.red, .green])
 		
 		XCTAssertNotNil(gauge.additionalInfo?.preTitle)
