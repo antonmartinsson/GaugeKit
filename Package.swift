592644138a69ae14fbfff4cfed7198dc3a08539d
Anton Martinsson <aejmartinsson@gmail.com>
2022-06-07 22:10:05 +0200

    Add Apple Watch support.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GaugeKit",
 	 platforms: [
-    .macOS(.v11), .iOS(.v13)
+		.macOS(.v11), .iOS(.v13), .watchOS(.v6)
   ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
--- Sources/GaugeKit/CrossPlatform.swift
+++ Sources/GaugeKit/CrossPlatform.swift
@@ -8,25 +8,29 @@
 import SwiftUI
 
 class CrossPlatform {
-   static func mainScreenSize() -> CGSize {
+	static func mainScreenSize() -> CGSize {
 #if os(macOS)
-      if let screenSize = NSScreen.main?.frame.size {
-         return screenSize
-      }
-      else {
-         // Use a generic FHD size so any calculations will still work
-         return CGSize(width: 1920, height: 1080)
-      }
+		if let screenSize = NSScreen.main?.frame.size {
+			return screenSize
+		}
+		else {
+			// Use a generic FHD size so any calculations will still work
+			return CGSize(width: 1920, height: 1080)
+		}
 #elseif os(iOS)
-      return UIScreen.main.bounds.size
+		return UIScreen.main.bounds.size
+#elseif os(watchOS)
+		return WKInterfaceDevice.current().screenBounds.size
 #endif
-   }
-   
-   static func systemBackgroundColor() -> Color {
+	}
+	
+	static func systemBackgroundColor() -> Color {
 #if os(macOS)
-      Color(NSColor.windowBackgroundColor)
+		Color(NSColor.windowBackgroundColor)
 #elseif os(iOS)
-      Color(UIColor.systemBackground)
+		Color(UIColor.systemBackground)
+#elseif os(watchOS)
+		Color.black
 #endif
-   }
+	}
 }
