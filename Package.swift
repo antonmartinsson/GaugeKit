41bf6a7df9aa540e633f7e4ed6822c73b8495599
Anton Martinsson <antonm@rtinsson.com>
2024-01-09 21:27:16 +0100

    Add support for visionOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "GaugeKit",
 	 platforms: [
-		.macOS(.v11), .iOS(.v13), .watchOS(.v6)
+        .macOS(.v11), .iOS(.v13), .watchOS(.v6), .visionOS(.v1)
   ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
