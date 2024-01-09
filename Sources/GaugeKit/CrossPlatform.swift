e144169ad0cb928c2715975f5828d9862c4a1a02
Anton Martinsson <antonm@rtinsson.com>
2024-01-09 21:46:53 +0100

    Fix errors

--- Sources/GaugeKit/CrossPlatform.swift
+++ Sources/GaugeKit/CrossPlatform.swift
@@ -8,27 +8,13 @@
 import SwiftUI
 
 class CrossPlatform {
-  static func mainScreenSize() -> CGSize {
-#if os(macOS)
-    if let screenSize = NSScreen.main?.frame.size {
-      return screenSize
-    }
-    else {
-      // Use a generic FHD size so any calculations will still work
-      return CGSize(width: 1920, height: 1080)
-    }
-#elseif os(iOS)
-    return UIScreen.main.bounds.size
-#elseif os(watchOS)
-    return WKInterfaceDevice.current().screenBounds.size
-#endif
-  }
-  
   static func systemBackgroundColor() -> Color {
 #if os(macOS)
     Color(NSColor.windowBackgroundColor)
 #elseif os(iOS)
     Color(UIColor.systemBackground)
+#elseif os(visionOS)
+      Color.primary
 #elseif os(watchOS)
     Color.black
 #endif
@@ -38,7 +24,9 @@ class CrossPlatform {
 #if os(macOS)
     Color(NSColor.labelColor)
 #elseif os(iOS)
-    Color(UIColor.label)
+    Color(.label)
+#elseif os(visionOS)
+      Color(.label)
 #elseif os(watchOS)
     Color.black
 #endif
