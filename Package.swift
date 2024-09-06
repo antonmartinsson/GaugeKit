d68a22ac902456e3371503608e720ca81a7e184a
Anton Martinsson <antonm@rtinsson.com>
2024-09-06 23:42:17 +0200

    Use Swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,18 +14,16 @@ let package = Package(
             name: "GaugeKit",
             targets: ["GaugeKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GaugeKit",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "GaugeKitTests",
-            dependencies: ["GaugeKit"]),
+            dependencies: ["GaugeKit"]
+        ),
     ]
 )
